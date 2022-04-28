382e38648b7fe80f8c4b0ec3b1bf43afc7c27647
JeremyOmwenga <100848946+JeremyOmwenga@users.noreply.github.com>
2022-04-28 09:08:16 +0300

    Add files via upload
    Updated personal website with CSS

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var quotes=
+{
+	"The ones who are crazy enough to think they can change the world, are the ones who do. - Steve Jobs",
+	"Die with memories, not dreams. - Anonymous",
+	"The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+	"If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
+	"Life is what happens when you're busy making other plans. -John Lennon",
+	"The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
+	"It is during our darkest moments that we must focus to see the light. -Aristotle",
+	"In the end, it's not the years in your life that count. It's the life in your years. -Abraham Lincoln",
+	"Never let the fear of striking out keep you from playing the game. -Babe Ruth",
+	"Life is really simple, but we insist on making it complicated. -Confucius",
+	"Love the life you live. Live the life you love. -Bob Marley"
+}
